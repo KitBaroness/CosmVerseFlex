af3d7030da04b4969925f50e1c0eaf3ede6c4275
NinjaAssPirate <118331769+KitBaroness@users.noreply.github.com>
2024-04-10 08:17:46 -0400

    pull into Pirate from kitbaroness

--- target/release/deps/my_library-d70ff4b0e66f72ae.d
+++ target/release/deps/my_library-d70ff4b0e66f72ae.d
@@ -1,7 +1,7 @@
-/home/baroness/FAFO/unster/target/release/deps/libmy_library-d70ff4b0e66f72ae.rmeta: src/main/rust/lib.rs
+/home/baroness/dev/unster/target/release/deps/libmy_library-d70ff4b0e66f72ae.rmeta: src/main/rust/lib.rs
 
-/home/baroness/FAFO/unster/target/release/deps/libmy_library-d70ff4b0e66f72ae.rlib: src/main/rust/lib.rs
+/home/baroness/dev/unster/target/release/deps/libmy_library-d70ff4b0e66f72ae.rlib: src/main/rust/lib.rs
 
-/home/baroness/FAFO/unster/target/release/deps/my_library-d70ff4b0e66f72ae.d: src/main/rust/lib.rs
+/home/baroness/dev/unster/target/release/deps/my_library-d70ff4b0e66f72ae.d: src/main/rust/lib.rs
 
 src/main/rust/lib.rs:
--- target/release/libmy_library.d
+++ target/release/libmy_library.d
@@ -1,1 +1,1 @@
-/home/baroness/FAFO/unster/target/release/libmy_library.rlib: /home/baroness/FAFO/unster/src/main/rust/lib.rs
+/home/baroness/dev/unster/target/release/libmy_library.rlib: /home/baroness/dev/unster/src/main/rust/lib.rs
