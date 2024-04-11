ce030e8774c4c8702a96f91b68cc79be1c2e1718
NinjaAssPirate <118331769+KitBaroness@users.noreply.github.com>
2024-04-11 00:18:37 -0400

    shut down hook for gracefull shutdown javalin and logger is used to record the access to the home page and the results of the account creation process.Record any errors that occur during the processing of data. Fixed CSV file interaction and creation.

--- src/main/kotlin/com/base/Unster.kt
+++ src/main/kotlin/com/base/Unster.kt
@@ -1,36 +1,26 @@
 package com.base
 
 import io.javalin.Javalin
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import io.javalin.http.staticfiles.Location
 import org.slf4j.LoggerFactory
-
-
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 fun main() {
     val logger = LoggerFactory.getLogger("com.base.UnsterKt")
-    
+
     val app = Javalin.create { config ->
-        // Set the location of static files
         config.staticFiles.add("/static", Location.CLASSPATH)
     }.start(8080)
 
     app.get("/") { ctx ->
-        // Log the access to the home page
         logger.info("Home page accessed")
-
-        // Redirect to the home.html page
         ctx.redirect("/home.html")
-
     }
 
     app.post("/process-data") { ctx ->
         try {
-            // The Rust binary expects a certain format for input.
-            // Collect this from the POST request's body.
             val inputData = ctx.body()
-            
             val rustProcess = ProcessBuilder("./build/bin/my_binary").start()
 
             rustProcess.outputStream.write(inputData.toByteArray())
@@ -58,31 +48,24 @@ fun main() {
             return@post
         }
 
-        // Additional validation can go here...
-
         try {
-            // Trowing an IOException
             CsvDatabase.writeUserToCsv(username, phone, email)
+            logger.info("Account created for user: $username")
             ctx.status(201).json(mapOf("message" to "Account created successfully"))
         } catch (e: Exception) {
+            logger.error("Failed to create account: ${e.message}")
             ctx.status(500).json(mapOf("message" to "An error occurred while creating the account."))
         }
     }
 
-    // add routes and logic
-
-    // Register a shutdown hook 
+    // Other routes and logic add here
 
     Runtime.getRuntime().addShutdownHook(Thread {
-        println("Shutdown signal received.")
-        app.stop() // Stops the Javalin server
-        // Add cleanup code here
+        logger.info("Shutdown signal received.")
+        app.stop()
     })
 
-
-        // Log the successful start of the application
-        logger.info("Application has started on port 8080")
-
+    logger.info("Application has started on port 8080")
 }
 
 // Implement the rest of the base class below needed
