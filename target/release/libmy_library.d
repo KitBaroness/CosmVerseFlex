0dad6716cb9f93769ce32f18a92696266dacab5f
NinjaAssPirate <118331769+KitBaroness@users.noreply.github.com>
2024-04-12 00:55:31 -0400

    corrected  config.staticFiles.add

--- target/release/libmy_library.d
+++
@@ -1,1 +0,0 @@
-/home/baroness/KIT/plug/FlexNet/target/release/libmy_library.rlib: /home/baroness/KIT/plug/FlexNet/src/main/rust/lib.rs
--- target/release/my_binary.d
+++
@@ -1,1 +0,0 @@
-/home/baroness/KIT/plug/FlexNet/target/release/my_binary: /home/baroness/KIT/plug/FlexNet/src/main/rust/bin/my_binary.rs /home/baroness/KIT/plug/FlexNet/src/main/rust/lib.rs
