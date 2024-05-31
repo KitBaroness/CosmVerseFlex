8ee1766289fbbbceb1eaa70de711f05003c5b75c
NinjaAssPirate <118331769+KitBaroness@users.noreply.github.com>
2024-05-31 02:58:32 -0400

    Needing to finish configuration files to communicate with TS but submobuled the CosmVerse Application

--- target/release/deps/termcolor-8a4087caebadbd42.d
+++ target/release/deps/termcolor-8a4087caebadbd42.d
@@ -1,7 +1,7 @@
-/home/baroness/KIT/Stack-Config/target/release/deps/libtermcolor-8a4087caebadbd42.rmeta: /home/baroness/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
+/home/ddd/Cosmos/CosmVerseFlex/target/release/deps/libtermcolor-8a4087caebadbd42.rmeta: /home/ddd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
 
-/home/baroness/KIT/Stack-Config/target/release/deps/libtermcolor-8a4087caebadbd42.rlib: /home/baroness/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
+/home/ddd/Cosmos/CosmVerseFlex/target/release/deps/libtermcolor-8a4087caebadbd42.rlib: /home/ddd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
 
-/home/baroness/KIT/Stack-Config/target/release/deps/termcolor-8a4087caebadbd42.d: /home/baroness/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
+/home/ddd/Cosmos/CosmVerseFlex/target/release/deps/termcolor-8a4087caebadbd42.d: /home/ddd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs
 
-/home/baroness/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs:
+/home/ddd/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termcolor-1.4.1/src/lib.rs:
